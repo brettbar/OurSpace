3e1f65ad0f3da7c698ba3c4ccdc0a73c5b17fa71
Andrew Flagstead <amf0212@gmail.com>
2019-04-25 23:31:53 -0700

    Started GUI

--- Motherland.java
+++ Motherland.java
@@ -4,14 +4,76 @@ import java.util.Map;
 import java.util.Scanner;
 import java.io.*;
 import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
-public class Motherland {
+public class Motherland extends JFrame {
     private ArrayList<Comrade> proletariat;
+
+    private JButton checkUser;
+    private JButton checkStatus;
+    private JButton dijsktra;
+    private JButton login;
+
+    public Motherland() {
+        setTitle("OurSpace");
+        setSize(800, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        checkUser = new JButton();
+        checkStatus = new JButton();
+        dijsktra = new JButton();
+        login = new JButton();
+
+        checkUser.addActionListener(new ButtonListener());
+        checkStatus.addActionListener(new ButtonListener());
+        dijsktra.addActionListener(new ButtonListener());
+        login.addActionListener(new ButtonListener());
+
+        JPanel panel = new JPanel();
+        panel.add(checkUser);
+        panel.add(checkStatus);
+        panel.add(dijsktra);
+        panel.add(login);
+        add(panel);
+
+        JPanel imagePanel = new JPanel();
+        JLabel label = new JLabel();
+        
+        imagePanel.add(label);
+        ImageIcon yourBoi = new ImageIcon("YourBoi.jpg");
+       // imagePanel.setIcon(yourBoi);
+        setVisible(true);
+
+
+
+
+    }
+
+    private class ButtonListener implements ActionListener { 
+        public void actionPerformed (ActionEvent e) {
+            String command = e.getActionCommand();
+
+            if (command.equals("checkUser"));
+                //check if user exists
+
+            if (command.equals("checkStatus"));
+                //check camraderie of two comrades
+
+            if (command.equals("dijkstra"));
+                //execute dijkstra algorithm 
+            
+            if (command.equals("login"));
+                //login as commissar
+
+        }
+    }
 
     public static void main(String[] args)
     {
-        proletariat = new ArrayList<Comrade>();
+        new Motherland();
     }
 
     public static void readAndPopulate(String filename) throws Exception
