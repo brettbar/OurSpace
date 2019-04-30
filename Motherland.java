3958e30591fe020b36a0c51b96daaaa8d9f6d6f5
Andrew Flagstead <amf0212@gmail.com>
2019-04-29 21:14:13 -0700

    progress on GUI

--- Motherland.java
+++ Motherland.java
@@ -17,41 +17,61 @@ public class Motherland extends JFrame {
     private JButton dijsktra;
     private JButton login;
 
-    public Motherland() {
-        setTitle("OurSpace");
-        setSize(800, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private JPanel buttons;
+    private JPanel imagePanel;
+    private JLabel label;
 
-        checkUser = new JButton();
-        checkStatus = new JButton();
-        dijsktra = new JButton();
-        login = new JButton();
+    public Motherland() {
+        setTitle("OurSpace, the proletariate's social network");
+        setSize(650, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        doButtons();
+        doImage();
+        //pack();
+        
+        add(buttons, BorderLayout.EAST);    
+        add(imagePanel, BorderLayout.WEST);
+
+        setVisible(true);
+
+    }
+
+    private void doButtons() {
+        checkUser = new JButton("Check for Comrade");
+        checkStatus = new JButton("Status");
+        dijsktra = new JButton("Comrade Dijkstra");
+        login = new JButton("Commissar");
 
         checkUser.addActionListener(new ButtonListener());
         checkStatus.addActionListener(new ButtonListener());
         dijsktra.addActionListener(new ButtonListener());
         login.addActionListener(new ButtonListener());
 
-        JPanel panel = new JPanel();
-        panel.add(checkUser);
-        panel.add(checkStatus);
-        panel.add(dijsktra);
-        panel.add(login);
-        add(panel);
-
-        JPanel imagePanel = new JPanel();
-        JLabel label = new JLabel();
-        
-        imagePanel.add(label);
-        ImageIcon yourBoi = new ImageIcon("YourBoi.jpg");
-       // imagePanel.setIcon(yourBoi);
-        setVisible(true);
-
-
-
-
+        buttons = new JPanel();
+        buttons.setLayout(new BoxLayout(buttons, BoxLayout.Y_AXIS));
+        buttons.add(checkUser);
+        buttons.add(checkStatus);
+        buttons.add(dijsktra);
+        buttons.add(login);
     }
 
+    private void doImage() {
+        imagePanel = new JPanel();
+        label = new JLabel();
+        
+        ImageIcon yourBoi = new ImageIcon("YourBoi.jpg");
+        label.setIcon(yourBoi);
+        imagePanel.add(label);
+    }
+
+    private void lazer() {
+        ImageIcon YourBoiL = new ImageIcon("YourBoiLazer.jpeg");
+        label.setIcon(YourBoiL);
+        //add(label);
+        System.out.println("Photo");
+    }
     private class ButtonListener implements ActionListener { 
         public void actionPerformed (ActionEvent e) {
             String command = e.getActionCommand();
@@ -65,8 +85,11 @@ public class Motherland extends JFrame {
             if (command.equals("dijkstra"));
                 //execute dijkstra algorithm 
             
-            if (command.equals("login"));
+            if (e.getSource() == login) {
+                lazer();
+            }
                 //login as commissar
+
 
         }
     }
